64ecfd9a88aa8493dd7333e476bd0abd4c40dd8a
Facu <facutomasr@gmail.com>
2023-11-15 14:38:32 -0300

    add html y css product-detail

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 app.get('/product-detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')))
+app.get('/login', (req, res) =>
+  res.sendFile(path.join(__dirname, 'views', 'login.html'))
+)
 
 
 /*server*/ 
