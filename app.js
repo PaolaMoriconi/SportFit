945108e8f5ebc20049c129d054c551c85dbc32cc
PaolaMoriconi <paomori18@gmail.com>
2023-11-15 11:40:29 -0300

    se suben cambios

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html
 app.get('/register', (req, res) =>
   res.sendFile(path.join(__dirname, 'views', 'register.html'))
 )
-app.get('/header', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views', 'hearder.html'))
+app.get('/login', (req, res) =>
+  res.sendFile(path.join(__dirname, 'views', 'login.html'))
 )
 
 
