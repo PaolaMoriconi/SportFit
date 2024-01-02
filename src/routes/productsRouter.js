d4fc71cde15fdbb442882ebc9b4eab68ec06b97e
PaolaMoriconi <paomori18@gmail.com>
2024-01-02 08:52:38 -0300

    se creo la ruta para crear nuevos productos, se agregaron las librerias de multer y uuid. Se agrego el metodo escribirJson

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { detail, edit, add, cart } = require('../controllers/productsController')
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+         cb(null,path.join(__dirname,'/../../public/images'))
+    },
+    filename:(req,file,cb)=>{
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.originalname + '-' + uniqueSuffix + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({storage});
+
+const { detail, edit, add, cart, store } = require('../controllers/productsController')
 
 
 router
 .get('/detail/:id', detail)
 .get('/editar/:id?', edit)
 .get('/agregar', add)
+.post('/agregar', upload.single('image'),store)
 .get('/cart', cart)
 
 module.exports = router;
