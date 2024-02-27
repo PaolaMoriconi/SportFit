9b3d3188138d3b7dd3df2f5c013882fdc50cf64f
paola moriconi <julian.eduardo.aquino@gmail.com>
2024-02-27 01:53:08 -0300

    se agregan los mensajes de errores en la vista de registro de productos

--- src/controllers/productsController/store.js
+++ src/controllers/productsController/store.js
@@ -1,37 +1,43 @@
-const { validationResult } = require("express-validator")
-const db = require("../../database/models")
-const { Op } = require("sequelize")
+const { validationResult } = require("express-validator");
+const db = require("../../database/models");
+const { Op } = require("sequelize");
 
 module.exports = (req, res) => {
-
-  const errors = validationResult(req)
-  const { image, imageBack } = req.files
+  const errors = validationResult(req);
+  const { image, imageBack } = req.files;
   console.log("files:", req.files);
 
-    
-    if (errors.isEmpty()) {
-      const { nombre, precio, descuento, talles, detalleProducto, categoria } =req.body
-      db.Product.create({
-        name:nombre,
-        price:precio,
-        discount:descuento,
-        description:detalleProducto,
-        image:image[0].filename,
-        category_id:categoria
-      })
-      .then(({id})=>{
-        db.Image.create({name:imageBack[0].filename,product_id:id}).then(() =>{
-          res.redirect(`/products`)
-        })
-      })
-      
-    } else {
-      console.log("Errores: ",errors.mapped());
-      res.render("/products", {
+  if (errors.isEmpty()) {
+    const { nombre, precio, descuento, talles, detalleProducto, categoria } =
+      req.body;
+    db.Product.create({
+      name: nombre,
+      price: precio,
+      discount: descuento,
+      description: detalleProducto,
+      image: image[0].filename,
+      category_id: categoria,
+    }).then(({ id }) => {
+      db.Image.create({ name: imageBack[0].filename, product_id: id }).then(
+        () => {
+          res.redirect(`/products`);
+        }
+      );
+    });
+  } else {
+    console.log("Errores: ", errors.mapped());
+    const colores = db.Color.findAll();
+    const categorias = db.Category.findAll();
+
+    Promise.all([colores, categorias]).then(([colores, categorias]) => {
+      console.log({ colores }, { categorias });
+      res.render("products/productAdd", {
         user: req.session.userLogin,
         old: req.body,
-        errors: errors.mapped()
+        errors: errors.mapped(),
+        colores,
+        categorias,
       });
-    }
-
-}
+    });
+  }
+};
--- src/validations/products-validator.js
+++ src/validations/products-validator.js
@@ -2,16 +2,16 @@ const { body } = require("express-validator");
 
 module.exports = [
   body("nombre").notEmpty().withMessage("Este campo es obligario").bail(),
-  body.custom((value) => {
-    if (!req.files.image) {
-      reject("Debe seleccionar imagenes para la portada del producto");
+  body("image").custom((value) => {
+    if (!req.files && !req.files.image) {
+      reject("Debe seleccionar una imagen para la portada del producto");
     }
-  }),
-  body.custom((value) => {
-    if (!req.files.imageBack) {
-      reject("Debe seleccionar una imagene secundaria para el producto");
+  }).withMessage("Debe seleccionar una imagen para la portada del producto"),
+  body("imageBack").custom((value) => {
+    if (!req.files && !req.files.imageBack) {
+      reject("Debe seleccionar una imagen secundaria para el producto");
     }
-  }),
+  }).withMessage("Debe seleccionar una imagene secundaria para el producto"),
   body("precio").notEmpty().withMessage("Este campo es obligario").bail(),
 
   body("talles").notEmpty().withMessage("Este campo es obligario").bail(),
