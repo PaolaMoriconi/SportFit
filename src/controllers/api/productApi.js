bad65c018c361b5ef4d706b978f684881edb067d
Eric <menaericdaniel@gmail.com>
2024-03-30 20:13:55 -0300

    aplica api para cambiar talles en el dashboard

--- src/controllers/api/productApi.js
+++ src/controllers/api/productApi.js
@@ -19,6 +19,46 @@ const getAllProducts = async (req,res) =>{
     }
 
 }
+
+const updateSizes = async (req,res) => {
+    try {
+
+        const {idProduct,idSize} = req.body
+        const checkSize = await db.ProductSize.findOne({
+            where : {
+                product_id : idProduct,
+                size_id : idSize
+            }
+        })
+
+        if(checkSize){
+            await db.ProductSize.destroy({
+                where : {
+                    product_id : idProduct,
+                    size_id : idSize
+                }
+            })
+        }else {
+            await db.ProductSize.create({
+                product_id : idProduct,
+                size_id : idSize
+            })
+        }
+
+        return res.status(200).json({
+            ok:true,
+            msg: "Modificación exitosa"
+        })
+        
+    } catch (error) {
+        return res.status(error.status || 500).json({
+            ok:false,
+            msg:error.message || "hubo un error"
+        })
+    }
+}
+
 module.exports={
-    getAllProducts
+    getAllProducts,
+    updateSizes
 }
